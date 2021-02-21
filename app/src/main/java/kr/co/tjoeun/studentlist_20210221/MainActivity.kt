2ed87601db80095ba09c766971d91882a2765c45
dennis <travlInfo4you@gmail.com>
2021-02-21 17:21:54 +0900

    학생 목록 추가

--- app/src/main/java/kr/co/tjoeun/studentlist_20210221/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/studentlist_20210221/MainActivity.kt
@@ -2,10 +2,27 @@ package kr.co.tjoeun.studentlist_20210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.studentlist_20210221.datas.Student
 
 class MainActivity : AppCompatActivity() {
+    // 실제 학생들을 담아줄 목록 변수.
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("조경진", 1988))
+        mStudentList.add(Student("강서영", 1990))
+        mStudentList.add(Student("김윤경", 1981))
+        mStudentList.add(Student("박은희", 1970))
+        mStudentList.add(Student("신정범", 1989))
+        mStudentList.add(Student("오이슬", 1994))
+        mStudentList.add(Student("이예슬", 1990))
+        mStudentList.add(Student("이지인", 1996))
+        mStudentList.add(Student("채명호", 1973))
+        mStudentList.add(Student("최원종", 1967))
+        mStudentList.add(Student("최윤정", 1987))
     }
 }
