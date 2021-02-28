a049e86f8f61fcaf5bc9fca7cf0912755038d23f
dennis <travlInfo4you@gmail.com>
2021-02-28 11:03:54 +0900

    롱클릭 추가

--- app/src/main/java/kr/co/tjoeun/studentlist_20210221/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/studentlist_20210221/MainActivity.kt
@@ -2,11 +2,17 @@ package kr.co.tjoeun.studentlist_20210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.studentlist_20210221.adapters.StudentAdapter
 import kr.co.tjoeun.studentlist_20210221.datas.Student
 
 class MainActivity : AppCompatActivity() {
     // 실제 학생들을 담아줄 목록 변수.
     val mStudentList = ArrayList<Student>()
+
+    //리스트뷰의 각각의 줄을 실제로 뿌려줄 어댑터 변수. 나중에 대입해주는 변수. onCreate가 실행된 후.
+    lateinit var mAdapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -24,5 +30,36 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(Student("채명호", 1973))
         mStudentList.add(Student("최원종", 1967))
         mStudentList.add(Student("최윤정", 1987))
+
+        // 나중에 담아준다던 mAdpater 에 실제 데이터 대입.
+        // 어떤 화면에서 쓸것이냐?
+        // 어떤 화면을 사용할거냐?
+        // 어떤 내용을 뿌릴것이냐?
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        // 리스트뷰의 어댑터로써 => mAdpater를 지정
+        studentListView.adapter = mAdapter
+
+        // 리스트뷰의 각 줄이 눌렸을 경우 대처.
+        studentListView.setOnItemClickListener {parent, view, position, id ->
+
+            // position : 몇번 줄이 눌렸는지 알려줌.
+            // 클릭된 학생의 이름을 토스트로..
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, clickedStudent.name, Toast.LENGTH_SHORT).show()
+        }
+
+        studentListView.setOnItemLongClickListener { parent, view, position, id ->
+            val std = mStudentList[position]
+
+            Toast.makeText(this, "${std.name} 길게 눌림", Toast.LENGTH_SHORT).show()
+
+            // true - 롱클릭 전용, false - 전용X,
+            return@setOnItemLongClickListener true
+        }
+
+
+
     }
 }
--- app/src/main/java/kr/co/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/studentlist_20210221/adapters/StudentAdapter.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.studentlist_20210221.R
 import kr.co.tjoeun.studentlist_20210221.datas.Student
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StudentAdapter(val mContext : Context,
                      val resId : Int,
@@ -15,14 +18,38 @@ class StudentAdapter(val mContext : Context,
     // student_list_item xml 을 가지고 => 코틀린에서 다룰 수 있게 도와주는 변수. (inf) 클래스(LayoutInflater)
     val inf = LayoutInflater.from(mContext)
 
+    // convertView : 재활용으로 사용할 뷰.
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
 
         if(tempRow == null) {
-            tempRow = inf.inflate(R.layout.student_list_item, root: null)
+            tempRow = inf.inflate(R.layout.student_list_item,  null)
         }
 
         val row = tempRow!!
+
+        // 화면에 뿌려지는 row를 일부 수정하고 나서 => return 되도록.
+        // 몇번째 줄에 뿌릴 row인지 파악하고 상황에 맞게 가공.
+        // mList 에서 위치(position)에 맞는 학새을 먼저 추출.
+        val student = mList[position]
+
+        // row변수(XML 모양)에서 => 필요한 텍스트뷰/이미지뷰 등등을 꺼내오자.(수기 작업)
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+        // student 의 값을 => 텍스트뷰들에 반영.
+        nameTxt.text = student.name
+
+
+        // 현재 나이를 구해서 반영해주고 싶을때...=> 현재년도 - 출생년도 + 1
+
+        // JAVA의 Calendar 객체 생성 => 기본값 :
+
+        val cal = Calendar.getInstance()
+        val age : Int = cal.get(Calendar.YEAR) - student.birthYear + 1
+        ageTxt.text = "(${age}세)"
+        //ageTxt.text = "(${student.birthYear}세)"
+
 
         return row
     }
